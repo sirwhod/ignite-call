ef80c62c18e8e87389c33633b42958ba490628d0
Rodrigo Brandao <rodrigo.brandao98@gmail.com>
2024-03-04 21:02:08 -0300

    class: Configuração do ESLint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { getCssText } from "@ignite-ui/react";
-import { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from '@ignite-ui/react'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       <body>
@@ -15,5 +22,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }

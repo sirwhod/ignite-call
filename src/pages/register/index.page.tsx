1a78fd9602b5e09493397ec0268860bb34621982
Rodrigo Brandao <rodrigo.brandao98@gmail.com>
2024-03-06 22:44:22 -0300

    class: Manipulando Cookies no Next

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -8,6 +8,7 @@ import { FormError } from '../home/components/FormError'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { api } from '@/src/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -50,8 +51,13 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
-    } catch (error) {
-      console.log(error)
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err?.response?.data?.message)
+        return
+      }
+
+      console.log(err)
     }
   }
 

affb7e11c714791e0b43e4741ee7e38e2b4a03a8
Rodrigo Brandao <rodrigo.brandao98@gmail.com>
2024-03-09 18:30:22 -0300

    class: Criando Adapter do Prisma

---
+++ src/lib/ath/prisma-adapter.ts
@@ -0,0 +1,82 @@
+import { Adapter } from 'next-auth/adapters'
+import { prisma } from '../prisma'
+
+export function PrismaAdapter(): Adapter {
+  return {
+    async createUser(user) {},
+
+    async getUser(id) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: { id },
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email!,
+        emailVerified: null,
+        username: user.username,
+        avatar_url: user.avatar_url!,
+      }
+    },
+
+    async getUserByEmail(email) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: { email },
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email!,
+        emailVerified: null,
+        username: user.username,
+        avatar_url: user.avatar_url!,
+      }
+    },
+
+    async getUserByAccount({ providerAccountId, provider }) {
+      
+    },
+
+    async updateUser(user) {
+      
+    },
+
+    async deleteUser(userId) {
+      
+    },
+
+    async linkAccount(account) {
+      
+    },
+
+    async unlinkAccount({ providerAccountId, provider }) {
+      
+    },
+
+    async createSession({ sessionToken, userId, expires }) {
+      
+    },
+
+    async getSessionAndUser(sessionToken) {
+      
+    },
+
+    async updateSession({ sessionToken }) {
+      
+    },
+
+    async deleteSession(sessionToken) {
+      
+    },
+
+    async createVerificationToken({ identifier, expires, token }) {
+      
+    },
+
+    async useVerificationToken({ identifier, token }) {
+      
+    },
+  }
+}

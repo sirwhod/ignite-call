7cecec789ca2b420e7a422735144482086d0856b
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-03-29 19:49:00 -0300

    class: Deploy no NEON

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -47,23 +47,26 @@ export default async function handle(
   })
 
   const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
-    SELECT 
-      EXTRACT(DAY FROM S.date) AS date, 
-      COUNT(S.date) AS amount,
-      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
-    FROM schedulings S 
-    
-    LEFT JOIN user_time_intervals UTI
-      ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
+  SELECT
+    EXTRACT(DAY FROM S.DATE) AS date,
+    COUNT(S.date),
+    ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
 
-    WHERE S.user_id = ${user.id}
-      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+  FROM schedulings S
 
-    GROUP BY EXTRACT(DAY FROM S.date),
-      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+  LEFT JOIN user_time_intervals UTI
+    ON UTI.week_day = EXTRACT(DOW FROM S.date + INTERVAL '1 day')
 
-    HAVING amount >= size
-  `
+  WHERE S.user_id = ${user.id}
+    AND EXTRACT(YEAR FROM S.date) = ${year}::int
+    AND EXTRACT(MONTH FROM S.date) = ${month}::int
+
+  GROUP BY EXTRACT(DAY FROM S.DATE),
+    ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+  HAVING
+    COUNT(S.date) >= ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60);
+`
 
   const blockedDates = blockedDatesRaw.map((item) => item.date)
 
